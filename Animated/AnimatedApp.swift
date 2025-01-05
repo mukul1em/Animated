40ebca5535421521a2df795ebb2bc62b34d5b6cc
mukul1em <mukulrt1997@gmail.com>
2025-01-05 16:35:10 -0500

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Mukul Rawat on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animated/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animated
+//
+//  Created by Mukul Rawat on 1/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
